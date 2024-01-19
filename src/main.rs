71daab457e6ac7ae92a2cc8cac133ad6fddfd9fc
Ppag <ppagdev@tuta.com>
2024-01-19 20:39:50 +0000

    input is appended to last input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rand::Rng;
+//use rand::Rng;
 use std::io;
 
 fn get_input() -> String {
@@ -10,11 +10,20 @@ fn get_input() -> String {
         .read_line(&mut input)
         .expect("Failed to read line");
 
+    input = input.trim().to_string();
+
     return input;
 }
 
 
 fn main() {
-    let input = get_input();
-    println!("Your input: {input}");
+    println!("Wrongculator is now active!");
+    let mut current_expression = String::new();
+
+    loop {
+        let input = get_input();
+        current_expression = format!("{current_expression}{input}");
+        println!("Your input: {input}");
+        println!("Your expression: {current_expression}");
+    };
 }
