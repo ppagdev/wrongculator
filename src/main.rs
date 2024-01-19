65e8883894b3be091ce735296d06eb0333360425
Ppag <ppagdev@tuta.com>
2024-01-19 19:23:03 +0000

    accept user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use rand::Rng;
+use std::io;
+
+fn get_input() -> String {
+    println!("Waiting on input...");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    return input;
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let input = get_input();
+    println!("Your input: {input}");
 }
